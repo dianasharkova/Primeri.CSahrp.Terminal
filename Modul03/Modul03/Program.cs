1a661ffa51bf7d2324ad2e307fe9b4afd015cc3e
Диана Шаркова <dianamilkova@yahoo.com>
2015-10-12 11:35:59 +0300

    Модул 03, Видео 03 - Математически операции

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -14,9 +14,23 @@ namespace Modul03
 			Console.Write ("> b= "); 
 			b = Convert.ToDouble (Console.ReadLine ());
 
+			//Форматиране с дименсия
 			string _format = "#0.00 m3";
+			Console.WriteLine ("\n\n>a / b = " + (a / b).ToString(_format));
 
-			Console.WriteLine ("\n\n>a / b = " + (a / b).ToString(_format) );
+			//Степенуване
+			c = Math.Pow (a, b);
+			Console.WriteLine ("\n\n>a ^ b = " + c.ToString(_format));
+
+			//Коренуване
+			c = Math.Sqrt ( b );
+			Console.WriteLine ("\n\n>b ^ 1/2 = " + c.ToString(_format));
+
+
+			//Закръгления
+			c = Math.Round ( a / b );
+			Console.WriteLine ("\n\n>(a / b) = " + c.ToString());
+
 		}
 	}
 }
